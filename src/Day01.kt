dce70bfed2f06946070f9ef1e73cefc4adb07abd
Marcin Kotz <marcin@kotz.pl>
2022-11-30 17:45:10 +0100

    feat: simple day config and asserts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,7 @@
 fun main() {
+    val day = 1
+    val testTarget = 3
+
     fun part1(input: List<String>): Int {
         return input.size
     }
@@ -8,10 +11,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day${day.toString().padStart(2, '0')}_test")
+    assertEquals(part1(testInput), testTarget)
 
-    val input = readInput("Day01")
+    val input = readInput("Day${day.toString().padStart(2, '0')}")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("aoc2022/src", "$name.txt")
     .readLines()
 
 /**
@@ -14,3 +14,8 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun assertEquals(value: Any, target: Any) {
+    if (value != target)
+        check(false) { "Expected $target got $value" }
+}
