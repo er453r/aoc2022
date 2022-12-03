e1a6660a8e8850c889a06227dcbd446d4bf2d61d
Marcin Kotz <marcin@kotz.pl>
2022-12-03 08:22:02 +0100

    feat: day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,17 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val lowerScoreOffset = 'a'.code - 1
+    val upperScoreOffset = 'A'.code - 1 - 26
+
+    fun Char.priority(): Int = this.code - (if (this.code > lowerScoreOffset) lowerScoreOffset else upperScoreOffset)
+
+    fun part1(input: List<String>): Int = input.sumOf { items ->
+        items.chunked(items.length / 2)
+            .map { it.toSet() }
+            .reduce { a, b -> a.intersect(b) }
+            .first()
+            .priority()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Int = input.chunked(3).sumOf { group ->
+        group.map { it.toSet() }
+            .reduce { a, b -> a.intersect(b) }
+            .first()
+            .priority()
     }
 
     test(
         day = 3,
-        testTarget1 = 6,
-        testTarget2 = 6,
+        testTarget1 = 157,
+        testTarget2 = 70,
         part1 = ::part1,
-        part2 = ::part2
+        part2 = ::part2,
     )
 }
