1b85bfe56947e0478ef76679d984ffe345e8d8bf
Marcin Kotz <marcin@kotz.pl>
2022-12-04 10:46:01 +0100

    feat: day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,16 +1,46 @@
 fun main() {
+    val inputLineRegex = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+
+        for(line in input){
+            val (start1, end1, start2, end2) = inputLineRegex.matchEntire(line)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $line")
+
+            val first = IntRange(start1.toInt(), end1.toInt()).toSet()
+            val second = IntRange(start2.toInt(), end2.toInt()).toSet()
+
+            if(first.containsAll(second) || second.containsAll(first))
+                sum += 1
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+
+        for(line in input){
+            val (start1, end1, start2, end2) = inputLineRegex.matchEntire(line)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $line")
+
+            val first = IntRange(start1.toInt(), end1.toInt()).toSet()
+            val second = IntRange(start2.toInt(), end2.toInt()).toSet()
+
+            if(first.intersect(second).isNotEmpty())
+                sum += 1
+        }
+
+        return sum
     }
 
     test(
         day = 4,
-        testTarget1 = 6,
-        testTarget2 = 6,
+        testTarget1 = 2,
+        testTarget2 = 4,
         part1 = ::part1,
         part2 = ::part2,
     )
