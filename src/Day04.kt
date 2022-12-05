2c5d651b146ecb967aefb917ab7b5beef2ef6b1d
Marcin Kotz <marcin@kotz.pl>
2022-12-05 17:03:35 +0100

    feat: use int ranges

--- src/Day04.kt
+++ src/Day04.kt
@@ -4,7 +4,7 @@ fun main() {
     fun lineToSets(line: String): Pair<Set<Int>, Set<Int>> {
         val (start1, end1, start2, end2) = inputLineRegex.findAll(line).map { it.value.toInt() }.toList()
 
-        return Pair(IntRange(start1, end1).toSet(), IntRange(start2, end2).toSet())
+        return Pair((start1..end1).toSet(), (start2..end2).toSet())
     }
 
     fun part1(input: List<String>): Int = input
