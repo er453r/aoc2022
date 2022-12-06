73da071a9922ee1edc2a51369e4823fe7e88d3e3
Marcin Kotz <marcin@kotz.pl>
2022-12-06 08:02:28 +0100

    feat: day 6 refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,6 @@
 fun main() {
-    fun findUniqueSequence(haystack:String, sequenceLength:Int):Int{
-        val queue = ArrayDeque<Char>()
-
-        for(i in haystack.indices){
-            queue.addLast(haystack[i])
-
-            if(queue.size > sequenceLength)
-                queue.removeFirst()
-
-            if(queue.toSet().size == sequenceLength)
-                return i + 1
-        }
-
-        return -1
+    fun findUniqueSequence(haystack: String, sequenceLength: Int): Int {
+        return sequenceLength + haystack.windowed(sequenceLength).indexOfFirst { it.toSet().size == sequenceLength }
     }
 
     fun part1(input: List<String>) = findUniqueSequence(input.first(), 4)
