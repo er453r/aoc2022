da6e0e82cecdbce07a65cdb6dc8993c1e56e193a
Marcin Kotz <marcin@kotz.pl>
2022-12-06 06:16:26 +0100

    feat: day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,16 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val queue = ArrayDeque<Char>()
+        val haystack = input.first()
+
+        for(i in 0 until haystack.length){
+            queue.addLast(haystack[i])
+
+            if(queue.size > 4)
+                queue.removeFirst()
+
+            if(queue.toSet().size == 4)
+                return i + 1
+        }
+
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val queue = ArrayDeque<Char>()
+        val haystack = input.first()
+
+        for(i in 0 until haystack.length){
+            queue.addLast(haystack[i])
+
+            if(queue.size > 14)
+                queue.removeFirst()
+
+            if(queue.toSet().size == 14)
+                return i + 1
+        }
+
+        return 0
     }
 
     test(
         day = 6,
-        testTarget1 = 6,
-        testTarget2 = 6,
+        testTarget1 = 7,
+        testTarget2 = 19,
         part1 = ::part1,
         part2 = ::part2,
     )
