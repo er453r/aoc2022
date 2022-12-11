baa443292aa26b0fc95c945bc5916384d552b4e9
Marcin Kotz <marcin@kotz.pl>
2022-12-11 09:52:40 +0100

    feat: day 12 template

---
+++ src/Day12.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    test(
+        day = 12,
+        testTarget1 = 6,
+        testTarget2 = 6,
+        part1 = ::part1,
+        part2 = ::part2,
+    )
+}
