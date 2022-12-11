2d7ca0a08f6955400819d9d834f416e6f600ac15
Marcin Kotz <marcin@kotz.pl>
2022-12-11 09:51:11 +0100

    feat: day 11 refactor

--- src/Day11.kt
+++ src/Day11.kt
@@ -11,120 +11,58 @@ fun main() {
 
     val inputLineRegex = """\d+""".toRegex()
 
-    fun part1(input: List<String>): Long {
+    fun monkeyBusiness(input: List<String>, rounds: Int, divisor: Int): Long {
         val monkeys = mutableListOf<Monkey>()
-        val monkeyInspections = mutableMapOf<Int, Int>()
+        val inspections = mutableMapOf<Int, Int>()
 
-        input.chunked(7).forEach {
-            monkeys.add(
-                Monkey(
-                    id = inputLineRegex.findAll(it[0]).map { it.value.toInt() }.first(),
-                    items = ArrayDeque(inputLineRegex.findAll(it[1]).map { it.value.toLong() }.toList()),
-                    operation = it[2].split("= old").last()[1],
-                    operationParam = it[2].split("= old").last().substring(3),
-                    testParam = inputLineRegex.findAll(it[3]).map { it.value.toInt() }.first(),
-                    onSuccessTarget = inputLineRegex.findAll(it[4]).map { it.value.toInt() }.first(),
-                    onFailedTarget = inputLineRegex.findAll(it[5]).map { it.value.toInt() }.first(),
-                )
-            )
-
-            monkeyInspections[inputLineRegex.findAll(it[0]).map { it.value.toInt() }.first()] = 0
-        }
-
-
-        repeat(20) {
-            monkeys.forEach { monkey ->
-                while (monkey.items.isNotEmpty()) {
-                    val worryLevel = monkey.items.removeFirst()
-
-                    monkeyInspections[monkey.id] = monkeyInspections[monkey.id]!! + 1
-
-                    val operationParameter: Long = if (monkey.operationParam == "old") worryLevel else monkey.operationParam.toLong()
-
-                    var newWorryLevel = when (monkey.operation) {
-                        '*' -> worryLevel * operationParameter
-                        '/' -> worryLevel / operationParameter
-                        '+' -> worryLevel + operationParameter
-                        '-' -> worryLevel - operationParameter
-                        else -> throw Exception("Unknown operation $monkey.operation")
-                    }
-
-                    newWorryLevel /= 3
-
-                    if (newWorryLevel % monkey.testParam == 0L)
-                        monkeys[monkey.onSuccessTarget].items.addLast(newWorryLevel)
-                    else
-                        monkeys[monkey.onFailedTarget].items.addLast(newWorryLevel)
-                }
+        input.chunked(7).forEach { line ->
+            Monkey(
+                id = inputLineRegex.findAll(line[0]).map { it.value.toInt() }.first(),
+                items = ArrayDeque(inputLineRegex.findAll(line[1]).map { it.value.toLong() }.toList()),
+                operation = line[2].split("= old").last()[1],
+                operationParam = line[2].split("= old").last().substring(3),
+                testParam = inputLineRegex.findAll(line[3]).map { it.value.toInt() }.first(),
+                onSuccessTarget = inputLineRegex.findAll(line[4]).map { it.value.toInt() }.first(),
+                onFailedTarget = inputLineRegex.findAll(line[5]).map { it.value.toInt() }.first(),
+            ).let {
+                monkeys.add(it)
+                inspections[it.id] = 0
             }
         }
-
-        println(monkeyInspections)
-
-        return monkeyInspections.values.sorted().takeLast(2).map { it.toLong() }.reduce(Long::times)
-    }
-
-    fun part2(input: List<String>): Long {
-        val monkeys = mutableListOf<Monkey>()
-        val monkeyInspections = mutableMapOf<Int, Int>()
-
-        input.chunked(7).forEach {
-            monkeys.add(
-                Monkey(
-                    id = inputLineRegex.findAll(it[0]).map { it.value.toInt() }.first(),
-                    items = ArrayDeque(inputLineRegex.findAll(it[1]).map { it.value.toLong() }.toList()),
-                    operation = it[2].split("= old").last()[1],
-                    operationParam = it[2].split("= old").last().substring(3),
-                    testParam = inputLineRegex.findAll(it[3]).map { it.value.toInt() }.first(),
-                    onSuccessTarget = inputLineRegex.findAll(it[4]).map { it.value.toInt() }.first(),
-                    onFailedTarget = inputLineRegex.findAll(it[5]).map { it.value.toInt() }.first(),
-                )
-            )
-
-            monkeyInspections[inputLineRegex.findAll(it[0]).map { it.value.toInt() }.first()] = 0
-        }
-
-        println("Got ${monkeys.size} monkeys")
-
-        monkeys.forEach { println("${it.id} - ${it.testParam}") }
 
         val maxDiv = monkeys.map { it.testParam }.toSet().reduce(Int::times)
 
-        repeat(10000) {
+        repeat(rounds) {
             monkeys.forEach { monkey ->
                 while (monkey.items.isNotEmpty()) {
                     val worryLevel = monkey.items.removeFirst()
 
-                    monkeyInspections[monkey.id] = monkeyInspections[monkey.id]!! + 1
+                    inspections[monkey.id] = inspections[monkey.id]!! + 1
 
                     val operationParameter: Long = if (monkey.operationParam == "old") worryLevel else monkey.operationParam.toLong()
 
-                    var newWorryLevel = when (monkey.operation) {
+                    val newWorryLevel = when (monkey.operation) {
                         '*' -> worryLevel * operationParameter
-                        '/' -> worryLevel / operationParameter
                         '+' -> worryLevel + operationParameter
-                        '-' -> worryLevel - operationParameter
                         else -> throw Exception("Unknown operation $monkey.operation")
-                    } % maxDiv
+                    } / divisor % maxDiv
 
-                    if (newWorryLevel % monkey.testParam == 0L)
-                        monkeys[monkey.onSuccessTarget].items.addLast(newWorryLevel)
-                    else
-                        monkeys[monkey.onFailedTarget].items.addLast(newWorryLevel)
+                    monkeys[if (newWorryLevel % monkey.testParam == 0L) monkey.onSuccessTarget else monkey.onFailedTarget].items.addLast(newWorryLevel)
                 }
             }
-
-            if(it <= 20 - 1)
-                println(monkeyInspections)
         }
 
-        return monkeyInspections.values.sorted().takeLast(2).map { it.toLong() }.reduce(Long::times)
+        return inspections.values.sorted().takeLast(2).map { it.toLong() }.reduce(Long::times)
     }
+
+    fun part1(input: List<String>) = monkeyBusiness(input, rounds = 20, divisor = 3)
+
+    fun part2(input: List<String>) = monkeyBusiness(input, rounds = 10000, divisor = 1)
 
     test(
         day = 11,
         testTarget1 = 10605,
-        testTarget2 = 2713310158,
+        testTarget2 = 2713310158L,
         part1 = ::part1,
         part2 = ::part2,
     )
